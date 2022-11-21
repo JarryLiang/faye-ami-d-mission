0b1df7ebc7fbafb7ecb2a2337941df7574392786
alibobo <alibobo@mobisocial.us>
2022-11-22 03:51:41 +0800

    force name

---
+++ status.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const express = require('express')
+const _MISSION_PORT = 3000;
+const app = express()
+app.use(express.json());
+
+const Mission = require("./mission/Mission");
+
+const client = require('prom-client');
+
+const guage = new client.Gauge({
+  name: 'chatRoomCount',
+  help: 'The metric provide the count of chatroom`s people',
+  labelNames: ['chat_id']
+});
+
+let count =1;
+
+app.get("/metrics",(req,res)=>{
+  const st=Mission.MissionApi.getMissionStatus();
+  const str=JSON.stringify(st,null,2)
+  res.send(str);
+});
+
+
+app.post("/startStatus",(req,res)=>{
+  Mission.MissionApi.triggerStatusMission();
+});
+
+app.post("/startComment",(req,res)=>{
+  Mission.MissionApi.triggerCommentMission();
+});
+
+app.post("/stopMission",(req,res)=>{
+  Mission.MissionApi.stopMission();
+});
+
+
+
+app.get("/",(req,res)=>{
+  const fn=path.resolve(__dirname,"./html/index.html");
+  res.sendFile(fn);
+});
+
+
+app.listen(_MISSION_PORT, () => {
+  console.log(`Example app listening on port ${_MISSION_PORT}`)
+});
+
+
+
+setTimeout(()=>{
+    Mission.MissionApi.triggerStatusMission().then(()=>{
+
+    }).catch((err)=>{
+
+    });
+},5000);
+
